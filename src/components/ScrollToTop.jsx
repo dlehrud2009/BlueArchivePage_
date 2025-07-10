5a969667a8ddf68ae24eb1b0df030ae95d8cef77
Cellia <ffffff.gogle@gmail.com>
2025-07-11 08:27:53 +0900

    BA8

--- src/components/ScrollToTop.jsx
+++ src/components/ScrollToTop.jsx
@@ -1,11 +1,13 @@
 import { useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigationType } from 'react-router-dom';
 
 export default function ScrollToTop() {
   const { pathname } = useLocation();
+  const navigationType = useNavigationType();
 
   useEffect(() => {
-    window.scrollTo({ top: 0, behavior: 'smooth' }); // 부드럽게 위로
+    // 뒤로가거나 앞으로 가기 (POP)일 때도 스무스하게 위로 스크롤
+    window.scrollTo({ top: 0, behavior: 'smooth' });
   }, [pathname]);
 
   return null;
